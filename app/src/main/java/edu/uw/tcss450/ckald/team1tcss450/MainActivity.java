f5db461225ea88c681950a6d1d10e78557c5954c
nalivaykom <nalim2@uw.edu>
2022-04-26 13:06:11 -0700

    testing push added comment

--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //test comment
     //artur
+    //michael comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
