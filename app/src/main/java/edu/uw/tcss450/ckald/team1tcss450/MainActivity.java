11b169457e9ea2a7a1804ae8ec57c6ce2dad9ca2
ArturNalivayko <arturnalivayko@gmail.com>
2022-04-26 13:04:59 -0700

    comment

--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //test comment
+    //artur
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
