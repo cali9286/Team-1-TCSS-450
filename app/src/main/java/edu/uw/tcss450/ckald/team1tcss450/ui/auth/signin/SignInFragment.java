5776c3f1dbc07a16bdfa81d1f3af1629027f87a8
Cliff Kaldveer <cliff.kaldveer@gmail.com>
2022-04-28 14:17:44 -0700

    Added color and string values, put together the xml for sign in, added PasswordValidator as a util and RequestQueueSingleton for io (example from lab 3), also added SignInViewModel.

--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/ui/auth/signin/SignInFragment.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/ui/auth/signin/SignInFragment.java
@@ -2,23 +2,20 @@ package edu.uw.tcss450.ckald.team1tcss450.ui.auth.signin;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
-import edu.uw.tcss450.ckald.team1tcss450.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SignInFragment extends Fragment {
 
+    public SignInFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 }
