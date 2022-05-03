adeac8a26b62f3c75ff2c7cd4b48f8f02d407407
Cliff Kaldveer <cliff.kaldveer@gmail.com>
2022-05-03 14:54:48 -0700

    no bugs remaining, bottom nav menu still not showing, app won't open either.

--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/MainActivity.java
@@ -29,6 +29,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        MainActivityArgs args = MainActivityArgs.fromBundle(getIntent().getExtras());
+
+        new ViewModelProvider(this,
+                new UserInfoViewModel.UserInfoViewModelFactory(args.getEmail(), args.getJwt())
+        ).get(UserInfoViewModel.class);
+
+        setContentView(R.layout.activity_main);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -39,7 +47,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        setContentView(R.layout.activity_main);
     }
 }
--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/model/UserInfoViewModel.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/model/UserInfoViewModel.java
@@ -4,59 +4,43 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.auth0.android.jwt.JWT;
-import edu.uw.tcss450.ckald.team1tcss450.R;
+public class UserInfoViewModel extends ViewModel {
 
-public class UserInfoViewModel extends ViewModel{
+    private final String mEmail;
+    private final String mJwt;
 
-    private UserInfoViewModel(JWT jwt) {
+    private UserInfoViewModel(String email, String jwt) {
+        mEmail = email;
         mJwt = jwt;
     }
 
-    /**
-     * Asks if the JWT stored in this ViewModel is expired.
-     *
-     * NOTE: This lab does not use this behavior but this can be useful in client-server
-     * implementations.
-     *
-     * @return true if the JWT stored in this ViewModel is expired, false otherwise
-     */
-    public boolean isExpired() {
-        return mJwt.isExpired(0);
-    }
-
-    /**
-     * Get the email address that is stored in teh payload of the JWT this ViewModel holds.
-     *
-     * @return the email stored in the JWT this ViewModel holds
-     * @throws IllegalStateException when the JWT stored in this ViewModel is expired (Will not
-     * happen in this lab)
-     */
     public String getEmail() {
-        if (!mJwt.isExpired(0)) {
-            return mJwt.getClaim("email").asString();
-        } else {
-            throw new IllegalStateException("JWT is expired!");
-        }
+        return mEmail;
     }
 
-    private final JWT mJwt;
+    public String getmJwt() {
+        return mJwt;
+    }
 
     public static class UserInfoViewModelFactory implements ViewModelProvider.Factory {
-        private final JWT jwt;
-        public UserInfoViewModelFactory(JWT jwt) {
+
+        private final String email;
+        private final String jwt;
+
+        public UserInfoViewModelFactory(String email, String jwt) {
+            this.email = email;
             this.jwt = jwt;
         }
+
         @NonNull
         @Override
         public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
             if (modelClass == UserInfoViewModel.class) {
-                return (T) new UserInfoViewModel(jwt);
+                return (T) new UserInfoViewModel(email, jwt);
             }
             throw new IllegalArgumentException(
                     "Argument must be: " + UserInfoViewModel.class);
         }
     }
-
 }
 
