b92c215349f907a8b2df5f48fa6a52b6c7e731b8
ArturNalivayko <arturnalivayko@gmail.com>
2022-05-30 18:37:38 -0700

    recycler view icons now update correctly. still some bugs to work out though.

--- app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/ui/HomeFragment.java
+++ app/src/main/java/edu/uw/tcss450/ckald/team1tcss450/ui/HomeFragment.java
@@ -1,16 +1,29 @@
 package edu.uw.tcss450.ckald.team1tcss450.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import edu.uw.tcss450.ckald.team1tcss450.R;
 import edu.uw.tcss450.ckald.team1tcss450.databinding.FragmentHomeBinding;
@@ -21,11 +34,57 @@ import edu.uw.tcss450.ckald.team1tcss450.model.UserInfoViewModel;
  */
 public class HomeFragment extends Fragment {
 
+    private Context mcontext = this.getContext();
+    private TextView homeTempTV;
+    private ImageView homeWeatherPicIV;
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mcontext = context;
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        homeTempTV = v.findViewById(R.id.idTVCurrentHOMEWeather);
+        homeWeatherPicIV =v.findViewById(R.id.idIVHomeCurrentPic);
+
+        getTemp();
+        return v;
+    }
+
+    void getTemp() {
+        String url = "https://api.weatherapi.com/v1/forecast.json?key=95203d195a4b434780402424221205&q=Tacoma&days=1&aqi=no&alerts=no";
+        RequestQueue RQ = Volley.newRequestQueue(mcontext);
+        JsonObjectRequest JsonOR = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    String currentTemp = response.getJSONObject("current").getString("temp_f");
+                    homeTempTV.setText("The Current temperature is ".concat(currentTemp).concat("°F"));
+
+                    String conditionImage = response.getJSONObject("current").getJSONObject("condition").getString("icon");
+                    Picasso.get().load("https:".concat(conditionImage)).into(homeWeatherPicIV);
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                homeTempTV.setText("That did not work");
+            }
+        });
+        //Something
+        RQ.add(JsonOR);
     }
 
     @Override
